823d056109959b7ddd47fe1a78371090af06c614
Iraz Irfan <iraz@mislbd.com>
2019-10-24 12:40:16 +0600

    Forms Done

---
+++ Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebApplication.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Calculate(Models.TestModel tm)
+        {
+            var an = new Models.AnswerModel();
+            an.Ann = tm.val1 + tm.val2;
+
+            return View("Answer", an);
+        }
+    }
+}
---
+++ Models/TestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class TestModel
+    {
+        public double val1 { get; set; }
+        public double val2 { get; set; }
+    }
+}
